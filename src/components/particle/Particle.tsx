8c5be6b67444914723e0682e7cfe8147e2f77a9e
arkulnid <arkulnid@gmail.com>
2021-09-10 13:44:59 +0500

    deleted:    components/particle 	new file:   components/particle/Particle.css 	new file:   components/particle/Particle.tsx 	new file:   components/particle/arkul.svg 	new file:   components/particle/particles.json

---
+++ src/components/particle/Particle.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import './Particle.css';
+import Particles from "react-tsparticles";
+import baseConfig from "./particles.json"
+import { ISourceOptions, Container } from "tsparticles";
+import "pathseg";
+import polygonMask from "./arkul.svg";
+
+const Particle: React.FC = () => {
+  const containerRef: React.RefObject<Container> = React.useRef(null);
+  const config = React.useRef(baseConfig);
+  config.current.polygon!.url = polygonMask
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      const polygonOpt: any = containerRef.current!.options.polygon;
+      containerRef.current!.loadTheme("destroying");
+
+      containerRef.current!.refresh();
+    }, 5000)
+    setTimeout(() => {
+      const polygonOpt: any = containerRef.current!.options.polygon;
+      polygonOpt.enable = false; 
+      containerRef.current!.loadTheme("main");     
+     
+      containerRef.current!.refresh();
+    }, 15000)
+  }, [baseConfig])
+
+  return (
+    <div>
+      <Particles
+      container={containerRef}
+      options={baseConfig as ISourceOptions} />
+    </div>
+  );
+}
+
+
+export default Particle;
