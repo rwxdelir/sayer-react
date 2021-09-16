795c676660de7abeee11c0e3b4d1b2a5ccffbc5a
arkulnid <arkulnid@gmail.com>
2021-09-16 20:53:49 +0500

    Add dark-breeze theme for terminal-window

--- src/components/window/Window.tsx
+++ src/components/window/Window.tsx
@@ -1,37 +1,39 @@
 import React, { Component, useState } from "react";
 import { Rnd } from "react-rnd";
-import "./Window.css"
+import "./Window.css";
+import konsole_icon from "./../../assets/images/konsole.png"
+import close_icon from "./../../assets/images/close.png"
 
 const Window: React.FC = () => { 
   const [width, setWidth] = useState(699);
   const [height, setHeight] = useState(436);
-  const [drag, setDrag] = useState(false);
-  React.useEffect(() => {
-    
-  }, [drag])
+  const [x, setX] = useState(565);
+  const [y, setY] = useState(250);
 
   return (
     <Rnd
-      className="window cancel-dragging"
+      className="window"
       size={{
         width: width,
         height: height
       }}
+      minWidth = {699}
+      minHeight = {436}
+      position = {{x: x, y: y}}
+      onDragStop={(e, d) => { setX(d.x);
+                              setY(d.y)}}
       onResize={(e, direction, ref, delta, position) => {
         setWidth(parseInt(ref.style.width));
         setHeight(parseInt(ref.style.height));
       }}
       cancel=".cancel-dragging"
-      position={{x: 581, y: 240}}
-//    resizeHandleClasses={{bottom: "window-bar", left: "window-bar"}}
-//      resizeHandleWrapperClass="resize-wrapper"
-      enableResizing={false}
-      resizeHandleClasses={{top: "resize-wrapper"}}
-      enableResizing={{top: true}}
-      resizeHandleWrapperStyle={{position: "absolute", width: width-16}}
+      dragHandleClassName="resize-wrapper"
+      enableResizing={true}
     >
-      <span className="resize-wrapper"></span>
-      <div className="window-bar cancel-dragging">
+      <span className="resize-wrapper" style={{width: width-10}}></span>
+      <img src={konsole_icon} class="terminal-icon" width="22" height="22"/>
+      <img src={close_icon} class="close-icon" width="25" height="25"/>
+      <div className="window-bar">
       <div><span className="window-cmd">arkul@localhost:~/home/arkul&gt;</span> grep -A10000 RU about.txt</div>
 Get into the creative mind of film director Lars von Trier and learn how reading in his world is connected with writing. Trier calls literature his “basic medium” and reveals his inspiration from writers as Thomas Mann, Leo Tolstoy, and Marcel Proust. He refers to dramaturgy as his “toothache” connected to his reading of Donald Duck, but writing is “the greatest kick you can get,” he says. 
 
