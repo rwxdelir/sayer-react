44beb20dbb4f4ff884699965ebe38daf26051702
arkulnid <arkulnid@gmail.com>
2021-11-26 18:03:27 +0500

    Responsive contactme (bootstrap)

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -10,53 +10,53 @@ import Btn from "./../../components/button/button";
 
 const Contact = () => {
   return (
-    <Container fluid className="contactme-icons" style={{position: "absolute", zIndex: 2}}>
-      <Row>
-        <Col className="aboutme">
-          <Col className="contact-title"> 
+    <Container fluid className="contactme-icons" style={{position:"absolute",zIndex:2,height:"100%"}}>
+      <Row style={{position:"relative", height:"100%", placeItems:"center",textAlign:"center"}}>
+        <Col className="aboutme justify-content-center order-1" xxl={6} xl={6} lg={6} md={6} sm={12}>
+          <Row className="contact-title"> 
             Contact Me
-          </Col>
-          <Col className="contact-description">
+          </Row>
+          <Row className="contact-description">
             I am available on almost every social media. You can message me, I will reply within 24 hours.
-          </Col>
-          <Row style={{display: "flex", marginTop: "30px"}}>
+          </Row>
+          <Row style={{display: "flex", marginTop: "30px", width: "450px"}}>
             <Col className="social-icon">
               <a href={"mailto:dinar.kulchurin@gmail.com"}>
                     <FaGoogle 
-                  style={{width: "65px", height: "65px", color: "white", background: "red", borderRadius: "100px", border: "10px solid red", marginRight: "20px"}}
+                  style={{width: "65px", height: "65px", color: "white", background: "red", borderRadius: "100px", border: "10px solid red"}}
                 />
               </a>
             </Col>
             <Col className="social-icon">
               <a href={"https://t.me/lero_ro"} target="_blank">
                 <FaTelegram 
-                  style={{width: "85px", height: "89px", color: "#0390d5", borderRadius: "100px",
-                    background: "#fff", marginRight: "20px"}}
+                  style={{width: "62px", height: "62px", color: "#0390d5", borderRadius: "100px",
+                    background: "#fff"}}
                 />
               </a>
             </Col>
             <Col className="social-icon">
               <a href={"https://vk.com/id152246572"} target="_blank">
                 <FaVk
-                  style={{width: "65px", height: "65px", color: "white", background: "#2787f5",
-                    borderRadius: "100px", border: "10px solid #2787f5", marginRight: "20px"}}
+                  style={{width: "61px", height: "61px", color: "white", background: "#2787f5",
+                    borderRadius: "100px", border: "10px solid #2787f5"}}
                 />
               </a>
             </Col>
             <Col className="social-icon">
               <a href={"https://github.com/arkulnid"} target="_blank">
                 <FaGithub
-                  style={{width: "50px", height: "50px", color: "white", border: "18px solid black",
-                    borderRadius: "500px", background: "black", marginRight: "0px !important"}}
+                  style={{width: "60px", height: "60px", color: "white", border: "18px solid black",
+                    borderRadius: "500px", background: "black"}}
                 />
               </a>
             </Col>
-            <div className="contact-btn">
-              <Btn title={"View Resume"} width={"170px"} href={"./#"} />
-            </div>
+          </Row>
+          <Row className="contact-btn justify-content-center">
+            <Btn title={"View Resume"} width={"170px"} href={"./#"} />
           </Row>
         </Col>
-        <Col className="message-svg-container" style={{position: "absolute", top: "150px", marginLeft: "30px"}}>
+        <Col className="message-svg-container" xxl={6} xl={6} lg={6} md={6} sm={12}>
           <MessageImg className="message-svg" />
         </Col>
       </Row>
