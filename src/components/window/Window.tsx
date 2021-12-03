ee1619de17eab13a537ae39ea791dd6fee5b927a
arkulnid <arkulnid@gmail.com>
2021-12-03 16:50:59 +0500

    Auto align terminal on center

--- src/components/window/Window.tsx
+++ src/components/window/Window.tsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useLayoutEffect, useRef } from "react";
 import { Rnd } from "react-rnd";
 import "./Window.css";
 import konsole_icon from "./../../assets/images/konsole.png";
@@ -12,6 +12,7 @@ const Window: React.FC = () => {
   const [height, setHeight] = useState(436);
   const [x, setX] = useState(565);
   const [y, setY] = useState(250);
+  const rnd: any = React.useRef();
 
   const _switchResolution = () => {
     const maximizeIcon: any = document.querySelector(".goup-icon");
@@ -41,75 +42,96 @@ const Window: React.FC = () => {
     menuKonsoleIcon.style.display="block";
   }
 
+  const _alignOnCenter = () => {
+    const windowClass: any = document.querySelector(".window");
+    let w = parseInt(windowClass.style.width)
+    let h = parseInt(windowClass.style.height)
+    const xx = Math.floor((window.innerWidth-w)/2);
+    const yy = Math.floor((window.innerHeight-h)/2);
+    rnd.current.updatePosition({ xx, yy });
+    setX(xx)
+    setY(yy)
+  }
+
+  React.useLayoutEffect(() => {
+    window.addEventListener('resize', _alignOnCenter)
+    return () => window.removeEventListener("resize", _alignOnCenter);
+  }, []);
+
   return (
-    <Rnd
-      className="window"
-      bounds="window"
-      size={{
-        width: width,
-        height: height
-      }}
-      minWidth={699}
-      minHeight={436}
-      maxWidth={window.innerWidth - 20}
-      maxHeight={window.innerHeight - 20}
-      position = {{x: x, y: y}}
-      onDragStop={(e, d) => { setX(d.x);
-                              setY(d.y)}
-      }
-      onResize={(e, direction, ref, delta, position) => {
-        setWidth(parseInt(ref.style.width));
-        setHeight(parseInt(ref.style.height));
-        setX(position.x);
-        setY(position.y);
-      }}
-      onResizeStop={(e, direction, ref, delta, position) => {
-        setX(position.x);
-        setY(position.y);
-      }}
-
-      cancel=".cancel-dragging"
-      dragHandleClassName="resize-wrapper"
-      enableResizing={true}
-    >
-      <span onDoubleClick={() => {
-        _switchResolution();
+    <div className="window-container" style={{width:window.innerWidth-10,height:window.innerHeight-10}}>
+      <Rnd
+        ref={rnd}
+        className="window"
+        bounds=".window-container"
+        size={{
+          width: width,
+          height: height
+        }}
+        minWidth={699}
+        minHeight={436}
+        maxWidth={window.innerWidth - 20}
+        maxHeight={window.innerHeight - 20}
+        position = {{x: x, y: y}}
+        onDragStop={(e, d) => { setX(d.x);
+                                setY(d.y)}
         }
-      } className="resize-wrapper" style={{width: width-9}}></span>
-      <img src={konsole_icon} className="terminal-icon" width="22" height="22"/>
-      <img src={close_icon} 
-        onClick = {() => { _closeWindow();}}
-        className="close-icon" 
-        width="26" height="26"
-      />
-      <img src={godown_icon} 
-          onClick = {() => {_closeWindow();}}
-          className="arrow-icon godown-icon" 
+        onResize={(e, direction, ref, delta, position) => {
+          setWidth(parseInt(ref.style.width));
+          setHeight(parseInt(ref.style.height));
+          setX(position.x);
+          setY(position.y);
+        }}
+        onResizeStop={(e, direction, ref, delta, position) => {
+          setWidth(parseInt(ref.style.width));
+          setHeight(parseInt(ref.style.height));
+          setX(position.x);
+          setY(position.y);
+        }}
+
+        cancel=".cancel-dragging"
+        dragHandleClassName="resize-wrapper"
+        enableResizing={true}
+      >
+        <span onDoubleClick={() => {
+          _switchResolution();
+          }
+        } className="resize-wrapper" style={{width: width-9}}></span>
+        <img src={konsole_icon} className="terminal-icon" width="22" height="22"/>
+        <img src={close_icon} 
+          onClick = {() => { _closeWindow();}}
+          className="close-icon" 
+          width="26" height="26"
+        />
+        <img src={godown_icon} 
+            onClick = {() => {_closeWindow();}}
+            className="arrow-icon godown-icon" 
+            width="13" height="13"
+        />
+        <img 
+          src={goup_icon} 
+          onClick = {() => {_switchResolution();}} 
+          className="arrow-icon goup-icon" 
           width="13" height="13"
-      />
-      <img 
-        src={goup_icon} 
-        onClick = {() => {_switchResolution();}} 
-        className="arrow-icon goup-icon" 
-        width="13" height="13"
-      />
-      <img 
-        src={diamond_icon} 
-        onClick = {() => {_switchResolution();}} 
-        className="diamond-icon goup-icon arrow-icon" 
-        width="17px" height="17px" />
-      <div className="window-bar">
-      <div><span className="window-cmd">arkul@localhost:~/home/arkul&gt;</span> grep -A10000 RU about.txt</div>
-Get into the creative mind of film director Lars von Trier and learn how reading in his world is connected with writing. Trier calls literature his “basic medium” and reveals his inspiration from writers as Thomas Mann, Leo Tolstoy, and Marcel Proust. He refers to dramaturgy as his “toothache” connected to his reading of Donald Duck, but writing is “the greatest kick you can get,” he says. 
+        />
+        <img 
+          src={diamond_icon} 
+          onClick = {() => {_switchResolution();}} 
+          className="diamond-icon goup-icon arrow-icon" 
+          width="17px" height="17px" />
+        <div className="window-bar">
+        <div><span className="window-cmd">arkul@localhost:~/home/arkul&gt;</span> grep -A10000 RU about.txt</div>
+  Get into the creative mind of film director Lars von Trier and learn how reading in his world is connected with writing. Trier calls literature his “basic medium” and reveals his inspiration from writers as Thomas Mann, Leo Tolstoy, and Marcel Proust. He refers to dramaturgy as his “toothache” connected to his reading of Donald Duck, but writing is “the greatest kick you can get,” he says. 
 
-Even though dramaturgy has been difficult for Lars von Trier, he experiences joy when writing the manuscripts for his films: “The greatest thing is when you let it go, like when you levitate in meditation. You do that sometimes when you’re writing. Then you write fabulously and you experience a high,” he says. “I hope I’ll manage to make a movie intuitively. But I’m stuck in the mire. The mire of dramaturgy.”
+  Even though dramaturgy has been difficult for Lars von Trier, he experiences joy when writing the manuscripts for his films: “The greatest thing is when you let it go, like when you levitate in meditation. You do that sometimes when you’re writing. Then you write fabulously and you experience a high,” he says. “I hope I’ll manage to make a movie intuitively. But I’m stuck in the mire. The mire of dramaturgy.”
 
-      <div className="window-cmd current-cmd">
-        arkul@localhost:~/home/arkul&gt;
-        <div className="konsole-cursor"></div>
+        <div className="window-cmd current-cmd">
+          arkul@localhost:~/home/arkul&gt;
+          <div className="konsole-cursor"></div>
+          </div>
         </div>
-      </div>
-    </Rnd>
+      </Rnd>
+    </div>
   )
 }
 
