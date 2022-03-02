2b17db0ac551f539ec5eb5d67021f3779780355c
arkulnid <arkulnid@gmail.com>
2022-03-02 19:16:41 +0500

    Append main theme for screens with small resolution

--- src/components/particle/Particle.tsx
+++ src/components/particle/Particle.tsx
@@ -1,33 +1,34 @@
 import React, { useEffect } from 'react';
 import './Particle.css';
 import Particles from "react-tsparticles";
-import baseConfig from "./particles.json"
+import polygonConfig from "./polygonConfig.json"
+import mainConfig from "./mainConfig.json";
 import { ISourceOptions, Container } from "tsparticles";
 import "pathseg";
 import polygonMask from "./arkul.svg";
 
 const Particle: React.FC = () => {
   const containerRef: React.RefObject<Container> = React.useRef(null);
-  const config = React.useRef(baseConfig);
-  config.current.polygon!.url = polygonMask
+  const config = React.useRef(mainConfig);
+  //config.current.polygon!.url = polygonMask
 
-// TODO: responsive loading theme
-// Solution: containerRef.current!.options.setResponsive 
+  // TODO: responsive loading theme
+  // Solution: containerRef.current!.options.setResponsive 
 
-  React.useEffect(() => {
-    setTimeout(() => {
-      const polygonOpt: any = containerRef.current!.options.polygon;
-      polygonOpt.enable = false; 
-      containerRef.current!.options.setTheme("main");    
-      containerRef.current!.refresh();
-    }, 3000)
-  }, [baseConfig])
+  //React.useEffect(() => {
+  //  setTimeout(() => {
+  //    const polygonOpt: any = containerRef.current!.options.polygon;
+  //    polygonOpt.enable = false; 
+  //    containerRef.current!.options.setTheme("main");    
+  //    containerRef.current!.refresh();
+  //  }, 3000)
+  //}, [baseConfig])
 
   return (
     <div>
       <Particles
       container={containerRef}
-      options={baseConfig as ISourceOptions} />
+      options={mainConfig as ISourceOptions} />
     </div>
   );
 }
